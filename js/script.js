9449d6c2c6879891ffc31763b25d517c8951b144
Amr A.Mohammed <merodiro@gmail.com>
2017-12-19 19:56:48 +0200

    add testimonials

--- js/script.js
+++ js/script.js
@@ -73,21 +73,39 @@ $(function () {
                 title: "Inavalid inputs!",
                 text: "Please fill inputs with the right value",
                 icon: "error",
-              });
+            });
         } else {
             swal({
                 title: "Success!",
                 text: "Thank you for contactiong us",
                 icon: "success",
-              });
+            });
         }
         e.preventDefault();
     });
 
     $('.scroll-to').click(function () {
         $('html, body').animate({
-          scrollTop: $('#' + $(this).data('value')).offset().top
+            scrollTop: $('#' + $(this).data('value')).offset().top
         }, 1000);
     });
 
+    (function autoSlider() {
+        $('.slider .active').each(function () {
+            if (!$(this).is(':last-child')) {
+                $(this).delay(3000).fadeOut(1000, function () {
+                    $(this).removeClass('active').next().fadeIn(1000).addClass('active');
+                    autoSlider();
+                });
+            } else {
+                $(this).delay(3000).fadeOut(1000, function () {
+                    $(this).removeClass('active');
+                    $('.slider div').eq(0).fadeIn(1000).addClass('active');
+                    autoSlider();
+                });
+            }
+        });
+    }());
+
 });
+s
